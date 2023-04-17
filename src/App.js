9a989e21a1e84b182143fe5053eec0541521f2c4
bofmar <bofmar@outlook.com>
2023-04-17 11:33:55 +0300

    create the main landmark components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import HeaderNav from './components/HeaderNav';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <HeaderNav />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/HeaderNav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HeaderNav() {
+  return (
+    <header>
+      <p>This is the header</p>
+    </header>
+  );
+}
