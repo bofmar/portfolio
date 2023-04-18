024fdaa949500982afb6435ad926c83addb70c62
bofmar <bofmar@outlook.com>
2023-04-18 19:28:27 +0300

    Add the data for the about section

--- src/data/TechnologyData.js
+++ src/data/TechnologyData.js
@@ -1,8 +1,17 @@
 import Technology from './Technology';
 
 const TechnologyData = [
-  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg', 'Linux'),
-  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg', 'Linux'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg', 'HTML5'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg', 'CSS3'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg', 'Javascript'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg', 'Typescript'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg', 'Sass'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg', 'React'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg', 'NodeJS'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg', 'Jest'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg', 'GIT'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg', 'Webpack'),
+  new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg', 'npm'),
   new Technology('https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg', 'Linux')
 ];
 
