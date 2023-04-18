743328e4558533e01b0de0299352d448cad4ffb6
bofmar <bofmar@outlook.com>
2023-04-18 18:34:02 +0300

    Update Project and ProjectData to include a stack array

--- src/data/Project.js
+++ src/data/Project.js
@@ -1,10 +1,11 @@
 export default class Project {
-  constructor(id, name, desc, imgPath, liveLink, codeLink) {
+  constructor(id, name, desc, imgPath, liveLink, codeLink, stack) {
     this.id = id;
     this.name = name;
     this.desc = desc;
     this.imgPath = imgPath;
     this.liveLink = liveLink;
     this.codeLink = codeLink;
+    this.stack = stack;
   }
 }
--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -2,8 +2,8 @@ import Project from './Project';
 import weather from '../assets/weather.gif';
 
 export const ProjectsData = [
-  new Project(0, 'Weather App', 'Placeholder', weather, 'https://bofmar.github.io/weather/', 'https://github.com/bofmar/weather'),
-  new Project(1, 'Weather App', 'Placeholder', weather, 'https://bofmar.github.io/weather/', 'https://github.com/bofmar/weather'),
-  new Project(2, 'Weather App', 'Placeholder', weather, 'https://bofmar.github.io/weather/', 'https://github.com/bofmar/weather'),
-  new Project(3, 'Weather App', 'Placeholder', weather, 'https://bofmar.github.io/weather/', 'https://github.com/bofmar/weather'),
+  new Project(0, 'Weather App', 'Placeholder', weather, 'https://bofmar.github.io/weather/', 'https://github.com/bofmar/weather', []),
+  new Project(1, 'Weather App', 'Placeholder', weather, 'https://bofmar.github.io/weather/', 'https://github.com/bofmar/weather', []),
+  new Project(2, 'Weather App', 'Placeholder', weather, 'https://bofmar.github.io/weather/', 'https://github.com/bofmar/weather', []),
+  new Project(3, 'Weather App', 'Placeholder', weather, 'https://bofmar.github.io/weather/', 'https://github.com/bofmar/weather', ['HTML5', 'SCSS', 'JavaScript']),
 ];
