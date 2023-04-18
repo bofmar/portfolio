ea33528cfb0b48cac21db7875669052f9f0aee0e
bofmar <bofmar@outlook.com>
2023-04-18 19:07:11 +0300

    Add text content to About section

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,9 @@ export default function About() {
     <section className='about-section' id='about'>
       <h3 className='section-title'>About</h3>
       <div className='about-left'>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis sed odio morbi quis commodo odio aenean. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. Ut tristique et egestas quis ipsum suspendisse ultrices gravida. Enim tortor at auctor urna nunc id cursus. Auctor elit sed vulputate mi sit amet mauris. Vestibulum lorem sed risus ultricies tristique nulla aliquet. Sodales neque sodales ut etiam. Erat nam at lectus urna duis. Parturient montes nascetur ridiculus mus mauris. Metus vulputate eu scelerisque felis. Semper risus in hendrerit gravida. Venenatis a condimentum vitae sapien pellentesque habitant morbi. Urna id volutpat lacus laoreet non curabitur. Lectus quam id leo in vitae. Est ultricies integer quis auctor elit sed vulputate. Est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque.</p>
+        <p>Hey there! I am Mario, a Web Developer who speciallizes in modern, performant, and responsive web apps.</p>
+        <p>I had a serious passion for technology ever since I was a wee lad, and instantly fell in love with programming ever since I wrote my first 'hello world'. After being introduced to Javascript, I became enthralled with all the posibilities web applications present to us, and now I am always on the look out for more oportunities to create new experiences, implement eye pleasing designs and make them pop with animations.</p>
+        <p>I am a person who firmly believes in continuous growth, and I constantly challenge my self with new technologies and ideas. I see myself as someone who is well organised, a problem solver who thrives under pressure, and with a good eye for detail. On my time off, I can be found gaming, listening to music or watching movies.</p>
       </div>
       <div className='about-right'>
         <SkillIcon path='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg' desc='Linux' />
