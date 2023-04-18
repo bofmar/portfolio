2822871c70f0a607de0980b46f8adced3469fadb
bofmar <bofmar@outlook.com>
2023-04-18 19:39:25 +0300

    Change icon descriptions to uppercase

--- src/components/SkillIcon.js
+++ src/components/SkillIcon.js
@@ -4,7 +4,7 @@ export default function SkillIcon({ path, desc }) {
   return (
     <div className='skill'>
       <img src={path} className='skill-icon' />
-      <h4 className='skill-description'>{desc}</h4>
+      <h4 className='skill-description'>{desc.toUpperCase()}</h4>
     </div>
   );
 }
