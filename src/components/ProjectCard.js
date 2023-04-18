cea1510b952c5ffb35fa235cbfd0d95695bc1979
bofmar <bofmar@outlook.com>
2023-04-18 18:42:41 +0300

    Add stack badges to projects on page

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,13 @@
 import React from "react";
+import StackBadge from './StackBadge';
 
 export default function ProjectCard({ project }) {
   return (
     <div className='project-card-wrapper'>
       <h4 className='project-name'>{project.name}</h4>
+      <section className='stack-section'>
+        {project.stack.map(tech => <StackBadge tech={tech} />)}
+      </section>
       <p className='project-description'>{project.desc}</p>
       <img src={project.imgPath} alt='' className='project-image' />
       <a href={project.liveLink} target='_blank' className='project-link'>Live app</a>
---
+++ src/components/StackBadge.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function StackBadge({ tech }) {
+  return (
+    <span className='stack-badge'>{tech}</span>
+  );
+}
