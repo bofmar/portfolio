22caae1e18de40173106472441e55a285d06b007
bofmar <bofmar@outlook.com>
2023-04-20 10:50:51 +0300

    Add social media section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ContactForm from './ContactForm';
+import SocialMedia from './SocialMedia';
 
 export default function Footer() {
   return (
     <footer id='contact'>
       <ContactForm />
+      <SocialMedia />
     </footer>
   );
 }
---
+++ src/components/SocialMedia.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SocialMedia() {
+  return (
+    <section className='social-media'>
+      <a target='_blank' href='https://github.com/bofmar'>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" className='github-icon' />
+      </a>
+      <a target='_blank' href='https://www.linkedin.com/in/marios-mpofilakis-03a1651b6/'>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" className='linkedin-icon' />
+      </a>
+    </section>
+  );
+}
