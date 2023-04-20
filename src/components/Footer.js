4a65d3e996211db20fd2f29030bec31c8742a418
bofmar <bofmar@outlook.com>
2023-04-20 11:09:49 +0300

    Add some text to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,14 @@ export default function Footer() {
   return (
     <footer id='contact'>
       <h3 className='section-title'>Contact Me</h3>
-      <ContactForm />
-      <SocialMedia />
+      <div className='contact-area'>
+        <h4>Would you like to get in touch? Please fill out the following form to send me a message and I'll get back to you the soonest possible.</h4>
+        <ContactForm />
+      </div>
+      <div className='socials-area'>
+        <h4>Or reach out to me via social media.</h4>
+        <SocialMedia />
+      </div>
     </footer>
   );
 }
