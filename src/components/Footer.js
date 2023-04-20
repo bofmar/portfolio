0e9de16f724a2e5378fe2584bfbdeb687cd79680
bofmar <bofmar@outlook.com>
2023-04-20 11:05:13 +0300

    Style the section headers uniformly

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import SocialMedia from './SocialMedia';
 export default function Footer() {
   return (
     <footer id='contact'>
+      <h3 className='section-title'>Contact Me</h3>
       <ContactForm />
       <SocialMedia />
     </footer>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ import { ProjectsData } from "../data/ProjectsData";
 export default function Projects() {
   return (
     <section className='projects-section' id='projects'>
-      <h3>Projects</h3>
+      <h3 className='section-title'>Projects</h3>
       <div className='projects-wrapper'>
         {ProjectsData.map(project => <ProjectCard project={project} key={project.id} />)}
       </div>
