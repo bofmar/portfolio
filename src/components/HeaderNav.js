915804eab86a260b3e1f6920afbdcfa849921605
bofmar <bofmar@outlook.com>
2023-04-22 19:12:23 +0300

    Change nav links to be capitalized

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -4,10 +4,10 @@ export default function HeaderNav() {
   return (
     <header>
       <ul>
-        <li><a href='#home'>Home</a></li>
-        <li><a href='#about'>About</a></li>
-        <li><a href='#projects'>Projects</a></li>
-        <li><a href='#contact'>Contact</a></li>
+        <li><a href='#home'>HOME</a></li>
+        <li><a href='#about'>ABOUT</a></li>
+        <li><a href='#projects'>PROJECTS</a></li>
+        <li><a href='#contact'>CONTACT</a></li>
       </ul>
     </header>
   );
