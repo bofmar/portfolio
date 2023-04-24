570e135de36843fa676ba20efa917e7ac9cc8f15
bofmar <bofmar@outlook.com>
2023-04-24 19:58:05 +0300

    Import react-cons and use the CgMenu from it

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,9 +1,11 @@
 import React from "react";
 import NavigationLinks from './NavigationLinks';
+import { CgMenu } from 'react-icons/cg';
 
 export default function HamburgerMenu() {
   return (
     <nav className='ham-nav'>
+      <CgMenu />
       <NavigationLinks />
     </nav>
   );
