941f48a2bbc157fe063ff959aa7bcc69bf4c115c
bofmar <bofmar@outlook.com>
2023-04-24 19:33:28 +0300

    Move the nav links to their own component

---
+++ src/components/NavigationLinks.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NavigationLinks() {
+  return (
+    <ul>
+      <li><a href='#home'>HOME</a></li>
+      <li><a href='#about'>ABOUT</a></li>
+      <li><a href='#projects'>PROJECTS</a></li>
+      <li><a href='#contact'>CONTACT</a></li>
+    </ul>
+  );
+}
