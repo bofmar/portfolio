b3a3db84aed6d13b0c6bf360f45b99c7588d4c0a
bofmar <bofmar@outlook.com>
2023-07-31 19:41:02 +0300

    Finish index, add back the favicon and the assets

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		port: 3000,
+	},
+	preview: {
+		port: 3000,
+	}
+})
